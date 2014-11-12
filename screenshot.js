050de64a5ce5dd78c44c8e9069128edbb5e488d4
Alvin Yuan <alvin.yuan92@gmail.com>
2014-11-11 22:47:43 -0800

    Add basic revisualization with y axis starting from 0 using D3.

--- screenshot.js
+++ screenshot.js
@@ -8,10 +8,64 @@ var topColors;
 var colorDistanceGroupTolerance = 20;
 var colorDistancePointTolerance = 5;
 
+var svgMargin = {top: 20, right: 20, bottom: 20, left: 70}
+var imageToDataScaleX, imageToDataScaleY;
+
 $(document).ready(function() {
   $("#generateViews").on("click", function(e) {
-    var axisStart = parseFloat($("#axis-start").val())
-    var axisEnd = parseFloat($("#axis-end").val())
+    var rgb = [topColors[1].r, topColors[1].g, topColors[1].b]
+    var points = extractRawDataForColor(rgb)
+
+    // Translate points from image pixel range to data range
+    var axisStart = parseFloat($("#axis-y-start").val())
+    var axisEnd = parseFloat($("#axis-y-end").val())
+    // TODO: Add way to specify largest value on top or on bottom
+    imageToDataScaleY.range([axisEnd, axisStart]) // flip to map 0 (pixel, i.e. top) to largest value
+    for (var i = 0; i < points.length; i++) {
+      var p = points[i]
+      p[0] = imageToDataScaleX(p[0])
+      p[1] = imageToDataScaleY(p[1])
+    }
+
+    // extractedData View
+    $("#extractedData").empty()
+    for (var i = 0; i < points.length; i++) {
+      var p = points[i]
+      var x = p[0].toFixed(2)
+      var y = p[1].toFixed(2)
+      $("<div>" + x + ", " + y + "</div>").appendTo($("#extractedData"))
+    }
+
+    // ReView1
+    // http://bl.ocks.org/mbostock/3883245
+    var svgSize = { width: imageWidth, height: imageHeight }
+    var dataToLinear0ScaleX = d3.scale.linear()
+      .domain([0, d3.max(points, function(d) { return d[0]; })])
+      .range([0, svgSize.width])
+    var dataToLinear0ScaleY = d3.scale.linear()
+      .domain([0, axisEnd])
+      .range([svgSize.height, 0]) // flip to map 0 to svg height (bottom)
+    var yAxis = d3.svg.axis()
+      .scale(dataToLinear0ScaleY)
+      .orient("left");
+    var line = d3.svg.line()
+      .x(function(d) { return dataToLinear0ScaleX(d[0]); })
+      .y(function(d) { return dataToLinear0ScaleY(d[1]); })
+
+    $("#ReView1").empty()
+    var svg = d3.select("#ReView1")
+      .attr("width", svgSize.width + svgMargin.left + svgMargin.right)
+      .attr("height", svgSize.height + svgMargin.top + svgMargin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + svgMargin.left + "," + svgMargin.top + ")");
+
+    svg.append("g")
+     .attr("class", "y axis")
+     .call(yAxis)
+    svg.append("path")
+      .datum(points)
+      .attr("class", "line")
+      .attr("d", line)
   });
 });
 
@@ -24,6 +78,14 @@ function setScreenshot(url, dim) {
   canvas.height = dim.height
   imageWidth = dim.width;
   imageHeight = dim.height;
+
+  // Default Scales
+  imageToDataScaleX = d3.scale.linear()
+    .domain([0, imageWidth])
+    .range([0, imageWidth]);
+  imageToDataScaleY = d3.scale.linear()
+    .domain([0, imageHeight])
+    .range([0, imageHeight]);
 
   imageObj.onload = function() {
     // Get image data by drawing screenshot onto canvas
@@ -57,7 +119,7 @@ function screenshotLoaded() {
 
 
 // color: [r, g, b], max=255
-function extractDataForColor(color) {
+function extractRawDataForColor(color) {
   var plotData = wpd.appData.getPlotData()
   var autoDetector = plotData.getAutoDetector()
 
@@ -66,7 +128,7 @@ function extractDataForColor(color) {
   // Run data extraction algorithm
   plotData.dataSeriesCol1 = [] // reset ActiveDataSeries
   autoDetector.generateBinaryData(); // needs colorDetectionMode and fgColor set
-  var algoCore = new wpd.AveragingWindowCore(autoDetector.binaryData, imageHeight, imageWidth, 5, 5, plotData.getActiveDataSeries())
+  var algoCore = new wpd.AveragingWindowCore(autoDetector.binaryData, imageHeight, imageWidth, 3, 3, plotData.getActiveDataSeries())
   algoCore.run()
   return plotData.getDataFromActiveSeries()
 }
@@ -87,11 +149,13 @@ function loadColorUI() {
 
   $("#colorUI").on('click', '.colorUI-choice', function(e) {
     a = $(e.target)
-    console.log(a)
     var i = parseInt(a.data('value'))
     var c = topColors[i]
     c = [c.r, c.g, c.b]
-    drawImageWithPoints(extractDataForColor(c))
+    drawImageWithPoints(extractRawDataForColor(c))
+
+    // Test Color Distances
+    console.log(a)
     for (var j = 0; j < topColors.length; j++) {
       var rgb = topColors[j]
       var rgb = [rgb.r, rgb.g, rgb.b]
